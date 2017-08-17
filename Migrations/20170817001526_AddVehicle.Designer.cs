b60ff2913fb8562063175d15054b36c85b3eaba6
mcockrell <mike.cockrell@gmail.com>
2017-08-16 20:16:50 -0400

    Adding vehicles model and supporting context with migrations

--- Migrations/20170817001526_AddVehicle.Designer.cs
+++ Migrations/20170817001526_AddVehicle.Designer.cs
@@ -8,9 +8,10 @@ using vega.Persistence;
 namespace Vega.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170817001526_AddVehicle")]
+    partial class AddVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
